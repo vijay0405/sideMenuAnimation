e95821bfb1964292c42ad33a45acc6a896f0bb54
vijay0405 <uvijaysimha@gmail.com>
2020-05-14 23:09:19 +0530

    switch between forward and backward animations

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const menuBtn = document.getElementById('menu');
 const sidebar = document.getElementById('sidebar');
 const content = document.getElementById('content');
+const checkBox = document.getElementById('myCheck');
 
 menuBtn.addEventListener('click', () => {
-	sidebar.classList.toggle('active');
-	content.classList.toggle('active');
+    sidebar.classList.toggle('active');
+    if (checkBox.checked == true){
+        content.classList.toggle('active');
+      } else {
+        content.classList.toggle('reverseActive');
+      }
+    
+    if(checkBox.style.display === "none") {
+        checkBox.style.display = "block";
+    } else {
+        checkBox.style.display = "none";
+    }
 });
 
